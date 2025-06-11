53cca658a0097e5aa4bced61237749091f03eb0b
Shakib0976 <web.shakib09@gmail.com>
2025-06-11 19:58:03 +0600

    added file structure this assignment

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,23 @@
+import {
+  createBrowserRouter,
+} from "react-router";
+import RootLayouts from "../Layouts/RootLayouts";
+import Home from "../Pages/Home/Home";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayouts,
+    children: [
+        {
+            index: true , 
+            Component : Home,
+        }
+    ]
+  },
+]);
+
+
+export default router ;
