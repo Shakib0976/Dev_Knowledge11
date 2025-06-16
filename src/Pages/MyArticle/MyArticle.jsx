a184fa6c3d3e894a9f9d460b528eccddfc745b25
Shakib0976 <web.shakib09@gmail.com>
2025-06-16 22:45:56 +0600

    delete article function implement

--- src/Pages/MyArticle/MyArticle.jsx
+++ src/Pages/MyArticle/MyArticle.jsx
@@ -1,9 +1,15 @@
 import React, { use, useEffect, useState } from 'react';
 import { AuthContext } from '../../Context/AuthContext';
+import { CiCalendarDate } from "react-icons/ci";
+import { HiPencilSquare } from "react-icons/hi2";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
 const MyArticle = () => {
     const { user } = use(AuthContext)
     const [article, setArticle] = useState([]);
+    console.log(article);
     console.log(user?.email);
 
     useEffect(() => {
@@ -17,44 +23,92 @@ const MyArticle = () => {
                 console.log(err);
             })
     }, [user])
+
+
+    const handleDelete = (id) => {
+
+
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axios.delete(`http://localhost:3000/allTask/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            const remainArticle = article.filter(art => art._id !== id);
+                            setArticle(remainArticle);
+                        }
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    })
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Your task has been deleted.",
+                    icon: "success"
+                });
+            }
+        });
+    }
+
+
+
+
     return (
         <div className='w-11/12 mx-auto my-10 '>
             <h1 className='text-2xl mt-15 mb-2 text-center md:text-5xl'>My Articles</h1>
-            <p className='text-center mb-6'>Manage your published articles</p>
+            <p className='text-center mb-8'>Manage your published articles</p>
             <div className='bg-gray-100 p-5'>
                 <div className="overflow-x-auto">
                     <table className="table">
                         {/* head */}
                         <thead>
                             <tr>
-                                <th>Article</th>
-                                <th>Category</th>
-                                <th>Date</th>
-                                <th>ACTIONS</th>
+                                <th className='text-lg'>Article</th>
+                                <th className='text-lg'>Category</th>
+                                <th className='text-lg'>Date</th>
+                                <th className='text-lg'>ACTIONS</th>
                             </tr>
                         </thead>
                         <tbody>
                             {/* row 1 */}
-                            <tr>
-                                <td>
-                                    <div className="flex items-center gap-3">
-                                        <div>
-                                            <div className="font-bold">Hart Hagerty</div>
-                                            <div className="text-sm opacity-50">United States</div>
+                            {
+                                article.map(art => <tr>
+                                    <td>
+                                        <div className="flex items-center gap-3">
+                                            <div className="avatar">
+                                                <div className="mask mask-squircle h-12 w-12">
+                                                    <img
+                                                        src={art?.url}
+                                                        alt="Avatar Tailwind CSS Component" />
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <div className="font-bold">{art.article}</div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    Zemlak, Daniel and Leannon
-                                    <br />
-                                    <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-                                </td>
-                                <td>Purple</td>
-                                <th>
-                                    <button className="btn btn-ghost btn-xs">details</button>
-                                </th>
-                            </tr>
-                            
+                                    </td>
+                                    <td>
+                                        <h1>{art.actegory}</h1>
+                                    </td>
+                                    <td className='flex items-center space-x-2'><CiCalendarDate size={20} />{art.date}</td>
+                                    <th>
+                                        <div className='space-x-4'>
+                                            <button><HiPencilSquare size={25} /></button>
+                                            <button onClick={() => handleDelete(art._id)}><RiDeleteBin6Line size={25} /></button>
+                                        </div>
+                                    </th>
+                                </tr>)
+                            }
+
                         </tbody>
 
                     </table>
