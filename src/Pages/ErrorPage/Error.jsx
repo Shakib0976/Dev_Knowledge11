3b61761ee1af175b454d0d80055c91d8c1f3ba65
Shakib0976 <web.shakib09@gmail.com>
2025-08-10 16:18:07 +0600

    change error page design

--- src/Pages/ErrorPage/Error.jsx
+++ src/Pages/ErrorPage/Error.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React from "react";
+import { Link } from "react-router";
+import Lottie from "lottie-react";
+import errorAnimation from "../../assets/404page.json"; 
 
 const ErrorPage = () => {
     return (
+        <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-b from-gray-900 to-black text-white px-4 relative overflow-hidden">
+            {/* Lottie Animation */}
+            <div className="w-[300px] md:w-[450px]">
+                <Lottie animationData={errorAnimation} loop={true} />
+            </div>
 
-        <div className='max-w-11/12 mx-auto'>
-            <div className='items-center mt-12 grid justify-center text-center'>
-                <div>
-                    <img className=' w-[450px]' src="https://i.ibb.co.com/nMCpVphs/oops-404-error-with-broken-robot-concept-illustration-114360-5529.jpg" alt="" />
-                </div>
-                <div>
-                    <h1 className='font-bold text-3xl text-yellow-300'>Lost in Knowledge?</h1>
-                    <p className='mt-4'>Oops! The page you are looking dosen't  exist</p>
-                </div>
-                <div className='mt-8'>
-                    <Link className='  text-white  bg-[#0EA106]   px-6 py-2' to='/'>Got To Home</Link>
-                </div>
+            {/* Text Section */}
+            <h1 className="mt-2 text-2xl md:text-5xl font-extrabold text-yellow-400 drop-shadow-lg">
+                Lost in Knowledge?
+            </h1>
+            <p className="mt-4 text-lg text-gray-300 max-w-md text-center">
+                Oops! The page you are looking for doesn’t exist or has been moved.
+            </p>
+
+            {/* Home Button */}
+            <Link
+                to="/"
+                className="mt-8 px-6 py-3 bg-green-600 hover:bg-green-700 rounded-full shadow-lg transition-all duration-300 hover:scale-105"
+            >
+                Go To Home
+            </Link>
+
+            {/* Decorative Glow */}
+            <div className="absolute top-0 left-0 w-full h-full pointer-events-none">
+                <div className="absolute -top-40 -left-40 w-80 h-80 bg-green-500 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-pulse"></div>
+                <div className="absolute -bottom-40 -right-40 w-80 h-80 bg-yellow-500 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-pulse"></div>
             </div>
         </div>
     );
